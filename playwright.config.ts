7f8263cb9a11abc87f39b2b3ac44f2f12d8cf124
felipeDurante <31037991+felipeDurante@users.noreply.github.com>
2025-10-15 18:55:35 +0000

    Ajusta configuração do Playwright e variáveis de ambiente para testes funcionais

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
-    baseURL: `http://127.0.0.1:8080`,
+    // baseURL: `http://127.0.0.1:8080`,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
@@ -67,9 +67,9 @@ export default defineConfig({
   // },
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'npm run start',
-    url: 'http://localhost:3000',
-    reuseExistingServer: !process.env.CI,
-  },
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://localhost:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
 });
