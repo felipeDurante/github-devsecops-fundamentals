dbfd252f3eb9ce28bdad0eb010aa2514b24688b4
felipeDurante <31037991+felipeDurante@users.noreply.github.com>
2025-10-15 18:56:47 +0000

    Ajusta configuração do Playwright e variáveis de ambiente para testes funcionais

--- playwright.config.js
+++ playwright.config.js
@@ -60,11 +60,11 @@ module.exports = defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-   webServer: {
-    command: `mkdocs serve --dev-addr ${process.env.TETRIS_APP_HOST}:${process.env.TETRIS_APP_PORT}`,
-    url: `http://${process.env.TETRIS_APP_HOST}:${process.env.TETRIS_APP_PORT}`,
-    reuseExistingServer: !process.env.CI,
-  },
+  //  webServer: {
+  //   command: `mkdocs serve --dev-addr ${process.env.TETRIS_APP_HOST}:${process.env.TETRIS_APP_PORT}`,
+  //   url: `http://${process.env.TETRIS_APP_HOST}:${process.env.TETRIS_APP_PORT}`,
+  //   reuseExistingServer: !process.env.CI,
+  // },
   //   webServer: {
   //   command: `mkdocs serve --dev-addr ${process.env.TETRIS_APP_HOST}:${process.env.TETRIS_APP_PORT} --site-dir ${process.env.TETRIS_APP_PATH}`,
   //   url: `http://${process.env.TETRIS_APP_HOST}:${process.env.TETRIS_APP_PORT}/${process.env.TETRIS_APP_PATH}`,
